9851314d9fafa1497eee554b48cfe4890c242169
Igor Amidzic <amidzicigor@yahoo.com>
2016-09-14 18:48:39 -0400

    Update rpsV2.js

--- RPS/rpsV2.js
+++ RPS/rpsV2.js
@@ -1,3 +1,11 @@
+function submitForm() {
+    var firstName = document.form1.userNameBox.value;
+    var userNameInfo = '<div>Welcome ' + firstName + '!</div>';
+    document.getElementById("form1").id = "form2"
+    document.getElementById("form2").innerHTML = "<h3>" + userNameInfo + "</h3>";
+    
+}
+
 //function when 'yes' is clicked
 function yesButton(){
 	//changes 'yesAndNo' div id to 'awesome' id 
@@ -5,9 +13,4 @@ function yesButton(){
 	//
 	document.getElementById("instructions").innerHTML = "<h3>Choose Rock, Paper, or Scissors.</h3>";
 
-}
-
-function userNameFunction {
-	var userName1 = document.userName.userNameBox.value;
-	document.getElementById("writeUserName").innerHTML += userName1;
 }
